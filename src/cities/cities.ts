99c27e240f7994d0c2a39dda77116f157432364b
kstdbrv <kstdbrv1@gmail.com>
2021-06-28 11:29:40 +0300

    Fix: Add type const to cities.ts

--- src/cities/cities.ts
+++ src/cities/cities.ts
@@ -1,15 +1,9 @@
-interface ICities {
-  name: string,
-  lat?: string,
-  disabled?: boolean,
-  hidden?:boolean
-};
 
-export const CITIES: ICities[] = [
-  { name: 'Select city', disabled: true, hidden: true },
-  { name: 'Самара', lat: '53.195873' },
-  { name: 'Тольятти', lat: '53.507836' },
-  { name: 'Саратов', lat: '51.533557' },
-  { name: 'Казань', lat: '55.796127' },
-  { name: 'Краснодар', lat: '45.035470' },
-];
+export const CITIES = [
+  { name: 'Select city', lat: '', disabled: true, hidden: true },
+  { name: 'Самара', lat: '53.195873', disabled: false, hidden: false },
+  { name: 'Тольятти', lat: '53.507836', disabled: false, hidden: false },
+  { name: 'Саратов', lat: '51.533557', disabled: false, hidden: false },
+  { name: 'Казань', lat: '55.796127', disabled: false, hidden: false },
+  { name: 'Краснодар', lat: '45.035470', disabled: false, hidden: false },
+] as const
